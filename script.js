e97a0b3beefe95964201df2eeb19d450c8b89cec
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-26 16:31:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+
+document.getElementById("voteForm").addEventListener("submit", function(e){
+	e.preventDefault();
+let age = document.getElementById("age").value.trim();
+let name = document.getElementById("name").value.trim();
+
+if(age === "" || name === ""){
+	alert("Please enter valid details")
+	return;
+}
+
+age = parseInt(age);
+
+let votePromise = new Promise((function(resolve, reject){
+	setTimeout(function(){
+		if(age > 18){
+			resolve();
+		}
+		else{
+			reject();
+		}
+	}, 4000)
+}))
+
+votePromise.then(function(){
+	alert(`Welcome, ${name}. You can vote.`)
+}).catch(function(){
+	alert(`Oh sorry Doe. You aren't old enough`);
+})
+
+})
